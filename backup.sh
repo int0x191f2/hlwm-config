e9552c5b4363d59f99affc3a88969b3f82b1d71d
Adam Long <macadam.long@gmail.com>
2015-02-16 18:10:15 +0000

    Switched xterm input to a dmenu system (lighter and looks better)

--- backup.sh
+++ backup.sh
@@ -7,18 +7,5 @@ hc() {
 if [ ! -d ~/.config/herbstluftwm/layout ];then
   mkdir ~/.config/herbstluftwm/layout;
 fi
-
-echo "Name of the layout:"
-read -r name
-if [ ! -e ~/.config/herbstluftwm/layout/$name ];then
-  echo "Writing layout"
-  hc dump > ~/.config/herbstluftwm/layout/$name
-else
-  echo "File exists. Overwrite? [y/N]"
-  read -r overwrite
-  if [[ ! -z $overwrite ]] || [[ $overwrite == "y" ]]; then
-    hc dump > ~/.config/herbstluftwm/layout/$name
-  else
-    exit
-  fi
-fi
+name=$(ls ~/.config/herbstluftwm/layout -1 | echo $(dmenu -i -l 20 -p Backup))
+hc dump > ~/.config/herbstluftwm/layout/$name
--- restore.sh
+++ restore.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 clear
-echo "[L]ist layouts or [a]pply a layout?"
-read -r action
-if [[ ! -z $action ]] || [[ $action == "a" ]]; then
-  echo "Name of layout to load?"
-  read -r layoutName
-  herbstclient load `herbstclient get_attr tags.focus.name` "$echo `cat ~/.config/herbstluftwm/layout/$layoutName`"
-else
-  ls ~/.config/herbstluftwm/layout/ -1
-  read
-fi
+name=$(ls ~/.config/herbstluftwm/layout -1 | echo $(dmenu -i -l 20 -p Restore))
+herbstclient load `herbstclient get_attr tags.focus.name` "$echo `cat ~/.config/herbstluftwm/layout/$name`"
