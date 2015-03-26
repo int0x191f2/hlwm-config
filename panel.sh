966de1aa3794cbdf4d09c5e64a7c77ee152f34a2
Adam Long <macadam.long@gmail.com>
2015-03-26 18:05:56 +0000

    added audio volume to panel (via amixer)

--- panel.sh
+++ panel.sh
@@ -117,7 +117,7 @@ hc pad $monitor $panel_height
         echo -n "$separator"
         echo -n "^bg()^fg() ${windowtitle//^/^^}"
         # small adjustments
-        right="^bg() $date $separator $(acpi | awk '{print $4}' | sed -e "s/,//g") $separator $(cat /proc/loadavg | awk '{print $2}') $separator $(ip addr | ag "inet " | tr "/" " " | awk '{if (NR!=1) {print $9,$2}}' | tr "\n" " ")"
+        right="^bg() $date $separator $(amixer get Master | grep % | awk '{print $4}' | sed -e 's/\[//g' | sed -e 's/\]//g') $separator $(acpi | awk '{print $4}' | sed -e "s/,//g") $separator $(cat /proc/loadavg | awk '{print $2}') $separator $(ip addr | ag "inet " | tr "/" " " | awk '{if (NR!=1) {print $9,$2}}' | tr "\n" " ")"
         right_text_only=$(echo -n "$right" | sed 's.\^[^(]*([^)]*)..g')
         # get width of right aligned text.. and add some space..
         width=$($textwidth "$font" "$right_text_only    ")
