76aed7ddded2a34988bf4a09c282e4549b7a7429
Adam Long <macadam.long@gmail.com>
2015-03-26 18:23:02 +0000

    moved scripts to a scripts directory, added arch.png to repo, added charging battery status in panel

---
+++ scripts/panel/battery.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ ! -h /sys/class/power_supply/BAT0 ];then
+  exit
+fi
+if [ "$(acpi -a | awk '{print $3}')" != 'on-line' ]
+then
+  echo "$(acpi | awk '{print $4}' | sed -e "s/,//g")"
+fi
+if [ "$(acpi -a | awk '{print $3}')" == 'on-line' ]
+then
+  echo "$(acpi | awk '{print $4}' | sed -e "s/,//g")>"
+fi
