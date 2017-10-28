c3770a326768a9dcd6ab938224e150dde9dedd9a
Adam Long <macadam.long@gmail.com>
2017-10-28 10:18:57 -0400

    added polybar config

--- scripts/misc/startup.sh
+++ scripts/misc/startup.sh
@@ -4,6 +4,9 @@ if [ ! -f ~/.xbindkeysrc ];then cp ~/.config/herbstluftwm/.xbindkeysrc ~/.xbindk
 if [ $(md5sum ~/.config/herbstluftwm/.xbindkeysrc|awk '{print $1}') != $(md5sum ~/.xbindkeysrc|awk '{print $1}') ];then
   cp ~/.config/herbstluftwm/.xbindkeysrc ~/.xbindkeysrc
 fi
+# backup polybar config
+cp ~/.config/polybar/config ~/.config/polybar/config.backup
+cp ~/.config/herbstluftwm/scripts/panel/polybar/config ~/.config/polybar/config
 # synclient Touchpadoff=1
 xbindkeys
 nm-applet
